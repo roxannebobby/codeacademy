abf865eed41c05cfe5181f1b0ea95d74eaedede3
Roxanne Weber <89050736+stackdev-web@users.noreply.github.com>
2021-09-22 17:12:56 -0500

    added .filter .includes iterator exercise

---
+++ function_findIndex.js
@@ -0,0 +1,25 @@
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+const foundAnimal = animals.findIndex(animal => {
+  return animal === 'elephant';
+});
+
+// console.log(foundAnimal)
+
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+// Something is missing in the method call below
+
+console.log(words.some((word) => {
+  return word.length < 6;
+}));
+
+// Use filter to create a new array
+const interestingWords = words.filter(word => word.length > 5);
+
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+console.log(interestingWords.every((word) => {
+  return word[0].length > 5; 
+}));
